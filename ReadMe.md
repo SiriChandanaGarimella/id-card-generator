52ec49edffca0305fefc0c6cbb20a7dad6140635
SiriChandanaGarimella <156231102+SiriChandanaGarimella@users.noreply.github.com>
2024-05-05 18:54:54 -0500

    added the config.properties file

---
+++ ReadMe.md
@@ -0,0 +1,26 @@
+This code base is to generate the id cards based on the given id template.
+
+All the properties should be given in config.properties file
+
+**config.properties file update**
+- **id_template** = Your id template location (Example: /Users/sirichandanagarimella/Documents/id_template.png)
+- **csv_file** = The csv file containing the details of name, title(position) and location of images (Example: /Users/sirichandanagarimella/Documents/id_data.csv)
+- **output_filename** = The name of the output file that you want it to be generated. Example (id_cards). The file will get generated in the id-card-generator folder.
+- **images_directory** = The directory containing the images (Example: /Users/sirichandanagarimella/Documents/images)
+
+**Commands to run the application:**
+In macOS:
+- Setup the venv environment: <code>python3 -m venv venv</code>
+- Activate the venv environment: <code>source venv/bin/activate</code>
+- Install the requirements: <code>pip install -r requirements.txt</code>
+
+In windows:
+- Setup the venv environment: <code>py -m venv venv</code>
+- Activate the venv environment: <code>.\venv\Scripts\activate</code>
+- Install the requirements: <code>pip install -r requirements.txt</code>
+
+Command to run the script:
+In macOS: <code>python3 app.py</code>
+In windows: <code>py app.py</code>
+
+
--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import os
 import csv
 import tempfile
-from reportlab.lib.pagesizes import landscape,portrait,letter
+from reportlab.lib.pagesizes import landscape
 from reportlab.platypus import SimpleDocTemplate, Image, PageBreak
 from reportlab.lib.units import inch
 from PIL import Image as PILImage, ImageDraw, ImageFont
+import configparser
 
-
-
-temp_file_paths=[]
+temp_file_paths = []
 custom_page_width = 255.0
 custom_page_height = 165.0
 custom_page_size = landscape((custom_page_width, custom_page_height))
+
+config = configparser.ConfigParser()
+config.read('config.properties')
+
+id_template_path = config['Properties']['id_template']
+csv_file_path = config['Properties']['csv_file']
+output_filename = config['Properties']['output_filename']+".pdf"
+images_directory = config['Properties']['images_directory']
 
 def create_temporary_image(pil_image):
     """
@@ -23,42 +30,37 @@ def create_temporary_image(pil_image):
         temp_file_paths.append(temp_file_path)
     return temp_file_path
 
-# Set up the PDF document
-output_filename = "id_cards.pdf"
+def truncate_name(name, max_length):
+    """
+    Truncate the name if it exceeds the maximum length.
+    """
+    if len(name) > max_length:
+        return name[:max_length]
+    else:
+        return name
+
 doc = SimpleDocTemplate(output_filename, pagesize=custom_page_size, leftMargin=0, rightMargin=0, topMargin=0, bottomMargin=0)
 elements = []
 
-# Open the CSV file and read the data
-with open("id_data.csv", "r") as csv_file:
+with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
-        # Load the ID template image
-        template_image = PILImage.open("id_template.png")
-        name_font = ImageFont.truetype("Roboto-Bold.ttf", size=30)
+        template_image = PILImage.open(id_template_path)
+        name_font = ImageFont.truetype("Roboto-Bold.ttf", size=20)
         title_font = ImageFont.truetype("Roboto-Bold.ttf", size=10)
-        # Add the name and title to the template image
-        name = row["Name"]
+        name = truncate_name(row["Name"], 29)
         title = row["Title"]
         draw = ImageDraw.Draw(template_image)
-        draw.text((8, 170), name, font=name_font, fill=(0, 0, 0))  # Adjust coordinates as needed
-        draw.text((175, 143), title, font=title_font, fill=(0, 0, 0))  # Adjust coordinates as needed
-
-        # Add the ID photo to the template image
+        draw.text((8, 170), name, font=name_font, fill=(0, 0, 0))
+        draw.text((175, 143), title, font=title_font, fill=(0, 0, 0))
         photo_path = row["Location"]
-        photo_image = PILImage.open(os.path.join("images", photo_path))
-        print(os.path.join("images", photo_path))
-        photo_image = photo_image.resize((100, 120))  # Adjust size as needed
-        template_image.paste(photo_image, (187, 20))  # Adjust coordinates as needed
-
-        # Save the template image to a temporary file
+        photo_image = PILImage.open(os.path.join(images_directory, photo_path))
+        photo_image = photo_image.resize((100, 120))
+        template_image.paste(photo_image, (187, 20))
         temp_file_path = create_temporary_image(template_image)
-
-        # Add the image to the PDF elements list
-        elements.append(Image(temp_file_path, 3.375 *inch, 2.125 *inch))  # Adjust size as needed
-
+        elements.append(Image(temp_file_path, 3.375 * inch, 2.125 * inch))
         elements.append(PageBreak())
 
-# Build the PDF document
 doc.build(elements)
 
 for temp_file in temp_file_paths:
